620251d4028624be210872d5a2a7ae4ad6ec92c6
sathana07 <sathanasana007@gmail.com>
2024-01-20 20:50:25 +0530

    personclass changes

--- StringConcepts/src/main/java/com/example/demo/application/CalculateArea.java
+++ StringConcepts/src/main/java/com/example/demo/application/CalculateArea.java
@@ -3,20 +3,20 @@ package com.example.demo.application;
 import java.util.Scanner;
 
 public class CalculateArea {
-	public void PrintArea(int side) {
+	public void printarea(int side) {
+
 		System.out.println("The area of the square is" + side * side);
 	}
 
-	public void PrintArea(int length, int breadth) {
+	public void printarea(int length, int breadth) {
 		System.out.println("The area of the Rectangle is" + length * breadth);
 	}
 
-	public void PrintArea(double factor, int base, int height) {
+	public void printarea(double factor, int base, int height) {
 		System.out.println("The area of the triangle is" + base * height * factor);
 	}
 
-	public void CalculateArea() {
-
+	public void calculatearea() {
 		try (Scanner userIn = new Scanner(System.in)) {
 			System.out.println("Enter 1 to calculate area of a square");
 			System.out.println("Enter 2 to calculate area of a Rectangle");
@@ -25,7 +25,7 @@ public class CalculateArea {
 			if (inval == 1) {
 				System.out.print("Enter side of the square");
 				int side = userIn.nextInt();
-				PrintArea(side);
+				printarea(side);
 			}
 
 			if (inval == 2) {
@@ -33,16 +33,17 @@ public class CalculateArea {
 				int length = userIn.nextInt();
 				System.out.print("Enter the breadth of the rectangle");
 				int breadth = userIn.nextInt();
-				PrintArea(length, breadth);
+				printarea(length, breadth);
 			}
 			if (inval == 3) {
 				System.out.print("Enter the height of the triangle");
 				int height = userIn.nextInt();
 				System.out.print("Enter the base of the triangle");
 				int base = userIn.nextInt();
-				PrintArea(height, base);
-			};
-			if (inval >3) {
+				printarea(0.5, height, base);
+			}
+			;
+			if (inval > 3) {
 				System.out.print("invalid input");
 			}
 
