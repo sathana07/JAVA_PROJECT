c1e091d5290e8abd8099835c2bb652997d773f8b
sathana07 <sathanasana007@gmail.com>
2024-01-20 19:52:33 +0530

    personclass changes

--- StringConcepts/src/main/java/Person.java
+++ StringConcepts/src/main/java/Person.java
@@ -4,29 +4,54 @@ private String name;
 private int age;
 private int  weight;
 private int height;
+/**
+ * @return the name
+ */
 public String getName() {
 	return name;
 }
+/**
+ * @param name the name to set
+ */
 public void setName(String name) {
 	this.name = name;
 }
+/**
+ * @return the age
+ */
 public int getAge() {
 	return age;
 }
+/**
+ * @param age the age to set
+ */
 public void setAge(int age) {
 	this.age = age;
 }
+/**
+ * @return the weight
+ */
 public int getWeight() {
 	return weight;
 }
+/**
+ * @param weight the weight to set
+ */
 public void setWeight(int weight) {
 	this.weight = weight;
 }
+/**
+ * @return the height
+ */
 public int getHeight() {
 	return height;
 }
+/**
+ * @param height the height to set
+ */
 public void setHeight(int height) {
 	this.height = height;
 }
+
 
 }
